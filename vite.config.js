fb51787456cdfd58bcfc0959f5a1bbe0f9ef5c2c
21120514 <nhann9642@gmail.com>
2024-10-07 22:47:53 +0700

    deploy github

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://github.com/nhanng9642/tic-tac-toe",
 })
+
